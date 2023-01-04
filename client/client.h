795a82be890621b1ce63091f7d68fba2a2b06488
hongsam14 <69339846+hongsam14@users.noreply.github.com>
2023-01-04 19:51:33 +0900

    basic client program

---
+++ client/client.h
@@ -0,0 +1,17 @@
+#ifndef CLIENT_H
+# define CLIENT_H
+
+//https://man7.org/linux/man-pages/man0/sys_socket.h.0p.html
+# include <sys/socket.h>
+//https://man7.org/linux/man-pages/man7/ip.7.html
+# include <netinet/in.h>
+
+# include <arpa/inet.h>
+
+# include <unistd.h>
+# include <stdio.h>
+# include <fcntl.h>
+
+
+
+#endif
